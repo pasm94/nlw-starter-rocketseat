4dd24659f1c034ce1f58405fb0507b40c02c93c0
Paulo Stein Müller <paulomuller_1994@hotmail.com>
2020-06-03 22:50:47 -0300

    aula 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -27,11 +27,14 @@ function getCities(event) { /* the event is the change, and it will be passed on
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = ''
+    citySelect.disabled = true
+
     fetch(url)
         .then(res => res.json()) /* response */
         .then(cities => {
             for (city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false
@@ -42,4 +45,52 @@ function getCities(event) { /* the event is the change, and it will be passed on
 document
     .querySelector('select[name=uf]')
     .addEventListener('change', getCities) /* keep listening the events. */
-    // getCities will be execute on change, and getCities() would be executed immediately
+// getCities will be execute on change, and getCities() would be executed immediately
+
+
+// Itens de coleta
+// get all li's
+
+const itemsToCollect = document.querySelectorAll('.items-grid li')
+
+for (const item of itemsToCollect) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector('input[name=items]')
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // add or remove a class with js
+    itemLi.classList.toggle('selected') /* toggle == if element has selected, toggle will remove, if not, toggle will add */
+
+    const itemId = itemLi.dataset.id
+
+    // check for selected items. If the are,
+    // get the selected items
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId /* compare and found. If it found, returns true */
+        return itemFound
+    })
+
+    // if already selected, remove
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+
+        // if not selected, add
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    // att the hidden field with the selected items
+    collectedItems.value = selectedItems
+}
